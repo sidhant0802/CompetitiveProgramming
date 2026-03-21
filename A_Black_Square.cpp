650bf8b376b143b8fdf45b4408067a90129baf47
sidhant <sidhantnirupam@gmail.com>
2026-03-21 23:25:00 +0530

    Add selected CP solutions

---
+++ A_Black_Square.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define in long long int
+#define vi vector<in>
+#define vii vector<vector<in>>
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define p(n) cout << n
+#define pn(n) cout << n << endl
+#define s(n) cin >> n
+#define f first
+#define sd second
+#define Y cout<<"Yes"<<endl
+#define N cout<<"No"<<endl
+#define fr(i, a, b) for (in i = a; i < (int)b; ++i)
+#define all(v) v.begin(), v.end()
+#define sz(v) (in)(v.size())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / __gcd(a, b)) * b
+
+void solve(){
+   in p,q;
+   s(p);s(q);
+   in x,y;
+   s(x);s(y);
+   in a=p;
+   in b=q;
+   p+=99;
+   q+=99;
+   if(x<=p&&x>=a&&y>=b&&y<=q){
+    Y;
+   }
+   else{
+    N;
+   }
+   return;
+}
+
+
+int main() {
+    fast;
+    in t;
+t=1;
+    while (t--) {
+        solve();
+    }
+}
